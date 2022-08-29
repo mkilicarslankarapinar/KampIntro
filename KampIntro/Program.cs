507a6e5766f28bdfaeb2ca03021cee55ff2d7a43
mkilicarslan <mkilicarslankarapinar@gmail.com>
2022-08-29 11:07:17 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //type safety - tip güvenliği
+            //Do no repeat yourself!
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+            
+
+
+        }
+    }
+}
